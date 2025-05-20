edc45378cbc35458e5ce5c3fb36f25fa6af21f66
casanera <kirdanangthang@gmail.com>
2025-05-21 02:05:44 +0300

    добавлен PostgreSQL через Docker и базовое подключение бд через Go

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/casanera/DlugoshSolutions
 
 go 1.24.3
+
+require github.com/lib/pq v1.10.9 // indirect
--- main.go
+++ main.go
@@ -1,23 +1,51 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
+var db *sql.DB // глобальная пер. для хранения объекта подключения к БД
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Тестирование сервера на Go")
+	// проверка подлкючения бд
+	err := db.Ping()
+	if err != nil {
+		http.Error(w, "Ошибка подключения к БД: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "Сервер работает и успешно подключился к PostgreSQL")
 }
 
 func main() {
-	http.HandleFunc("/", homeHandler)
+	dbHost := "localhost"
+	dbPort := "5432"
+	dbUser := "myuser"
+	dbPassword := "mypassword"
+	dbName := "mydatabase"
 
-	// порт, на котором будет работать сервер
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPassword, dbName)
+
+	var err error
+	db, err = sql.Open("postgres", connStr) // открываем соединение
+	if err != nil {
+		log.Fatalf("Ошибка при подключении к БД: %v", err)
+	}
+
+	err = db.Ping() // пажилая проверка что соединение действительно установлено
+	if err != nil {
+		log.Fatalf("Не удалось установить соединение с БД: %v", err)
+	}
+	fmt.Println("Успешное подключение к PostgreSQL")
+
+	http.HandleFunc("/", homeHandler)
 	port := ":8080"
 	fmt.Printf("Сервер запускается на порту %s\n", port)
-
-	// сообщение об ошибке, если возникнет
 	if err := http.ListenAndServe(port, nil); err != nil {
 		log.Fatal(err)
 	}
